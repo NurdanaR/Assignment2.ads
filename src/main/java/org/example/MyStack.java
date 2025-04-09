09cf4b208bf45eb97d449a4b2c5ac80367611958
Nurdana <241349@astanait.edu.kz>
2025-04-09 17:56:01 +0500

    класс MyStack, включает методы для добавления, удаления элементов и проверки состояния стека (пустой или нет)

---
+++ src/main/java/org/example/MyStack.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class MyStack<T> {
+    private MyArrayList<T> list = new MyArrayList<>();
+
+    public void push(T element) {
+        list.add(element);
+    }
+
+    public T pop() {
+        if (list.isEmpty()) throw new RuntimeException("Stack is empty");
+        return list.remove(list.size() - 1);
+    }
+
+    public T peek() {
+        if (list.isEmpty()) throw new RuntimeException("Stack is empty");
+        return list.get(list.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
+}
