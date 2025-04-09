dbb6a2dba77f9d17574b838e6bb23fae54bf5336
Nurdana <241349@astanait.edu.kz>
2025-04-09 16:32:16 +0500

    уточнение методов интерфейса Mylist

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,4 +1,13 @@
 package org.example;
 
-public class MyList {
+public interface MyList<T> extends Iterable<T> {
+    void add(T element);
+    void add(T element, int index);
+    T get(int index);
+    T remove(int index);
+    boolean remove(T element);
+    int size();
+    boolean isEmpty();
+    void clear();
+    boolean contains(T element);
 }
