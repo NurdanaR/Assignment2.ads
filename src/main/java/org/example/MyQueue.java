2a9e4126f26212c39be11f8e63b84bc53a0aee9a
Nurdana <241349@astanait.edu.kz>
2025-04-09 18:35:19 +0500

    реализован класс MyQueue

---
+++ src/main/java/org/example/MyQueue.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class MyQueue<T> {
+    private MyLinkedList<T> list = new MyLinkedList<>();
+
+    public void enqueue(T element) {
+        list.add(element);
+    }
+
+    public T dequeue() {
+        if (list.isEmpty()) throw new RuntimeException("Queue is empty");
+        return list.remove(0);
+    }
+
+    public T peek() {
+        if (list.isEmpty()) throw new RuntimeException("Queue is empty");
+        return list.get(0);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
+}
