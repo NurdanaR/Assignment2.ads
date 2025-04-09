841eba3e847d12d4d0eb68f40f3d8f0337cf57a6
Nurdana <241349@astanait.edu.kz>
2025-04-09 16:24:05 +0500

    Add MyList interface with basic list methods

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(10);
+        stack.push(20);
+        System.out.println("Stack pop: " + stack.pop()); // 20
+
+        MyQueue<String> queue = new MyQueue<>();
+        queue.enqueue("A");
+        queue.enqueue("B");
+        System.out.println("Queue dequeue: " + queue.dequeue()); // A
+
+        MyMinHeap<Integer> heap = new MyMinHeap<>();
+        heap.add(5);
+        heap.add(2);
+        heap.add(8);
+        System.out.println("Min element: " + heap.poll()); // 2
+    }
+}
