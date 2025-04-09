8afb4a02d7804f438f2c9811faf41d57e48b7c00
Nurdana <241349@astanait.edu.kz>
2025-04-09 18:40:57 +0500

    изменение в main и MyArrayList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(10);
+        stack.push(20);
+        System.out.println("Stack pop: " + stack.pop()); // 20
+
+        MyQueue<String> queue = new MyQueue<>();
+        queue.enqueue("A");
+        queue.enqueue("B");
+        System.out.println("Queue dequeue: " + queue.dequeue()); // A
+
+        MyMinHeap<Integer> heap = new MyMinHeap<>();
+        heap.add(5);
+        heap.add(2);
+        heap.add(8);
+        System.out.println("Min element: " + heap.poll()); // 2
     }
 }
--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -1,90 +1,97 @@
 package org.example;
 
 public class MyArrayList<T> implements MyList<T> {
-    private Object[] data;
+    private Object[] array;
     private int size;
-    private static final int DEFAULT_CAPACITY = 10;
 
     public MyArrayList() {
-        data = new Object[DEFAULT_CAPACITY];
+        array = new Object[10];  // начальный размер массива
         size = 0;
     }
 
-    private void ensureCapacity() {
-        if (size >= data.length) {
-            Object[] newData = new Object[data.length * 2];
-            System.arraycopy(data, 0, newData, 0, data.length);
-            data = newData;
-        }
-    }
-
+    @Override
     public void add(T element) {
-        ensureCapacity();
-        data[size++] = element;
+        if (size == array.length) {
+            resize();
+        }
+        array[size++] = element;
     }
 
-    public void add(int index, T element) {
-        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
-        ensureCapacity();
-        System.arraycopy(data, index, data, index + 1, size - index);
-        data[index] = element;
+    @Override
+    public void add(T element, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = element;
         size++;
     }
 
+    @Override
     public T get(int index) {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
-        return (T) data[index];
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+        return (T) array[index];
     }
 
+    @Override
     public T remove(int index) {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
-        T removed = (T) data[index];
-        System.arraycopy(data, index + 1, data, index, size - index - 1);
-        size--;
-        return removed;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+        T removedElement = (T) array[index];
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[--size] = null;
+        return removedElement;
     }
 
+    @Override
     public boolean remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (data[i].equals(element)) {
-                remove(i);
+            if (array[i].equals(element)) {
+                for (int j = i; j < size - 1; j++) {
+                    array[j] = array[j + 1];
+                }
+                array[--size] = null;
                 return true;
             }
         }
         return false;
     }
 
+    @Override
     public int size() {
         return size;
     }
 
+    @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
+    @Override
     public void clear() {
         size = 0;
-        data = new Object[DEFAULT_CAPACITY];
     }
 
+    @Override
     public boolean contains(T element) {
         for (int i = 0; i < size; i++) {
-            if (data[i].equals(element)) return true;
+            if (array[i].equals(element)) {
+                return true;
+            }
         }
         return false;
     }
 
-    public java.util.Iterator<T> iterator() {
-        return new java.util.Iterator<T>() {
-            private int current = 0;
-
-            public boolean hasNext() {
-                return current < size;
-            }
-
-            public T next() {
-                return (T) data[current++];
-            }
-        };
+    private void resize() {
+        Object[] newArray = new Object[array.length * 2];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
     }
 }
