ebdad8c5d94c44dbb6f31a85f5750fc063518a52
Nurdana <241349@astanait.edu.kz>
2025-04-11 16:53:50 +0500

    попытка 2

--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -1,95 +1,117 @@
 package org.example;
 
+import java.util.Iterator;
+
 public class MyArrayList<T> implements MyList<T> {
-    private Object[] data;
-    private int size;
-    private static final int DEFAULT_CAPACITY = 10;
-
-    public MyArrayList() {
-        data = new Object[DEFAULT_CAPACITY];
-        size = 0;
-    }
-
-    private void ensureCapacity() {
-        if (size >= data.length) {
-            Object[] newData = new Object[data.length * 2];
-            System.arraycopy(data, 0, newData, 0, data.length);
-            data = newData;
-        }
-    }
+    private Object[] elements = new Object[100];
+    private int size = 0;
 
     public void add(T element) {
-        ensureCapacity();
-        data[size++] = element;
-    }
-
-    @Override
-    public void add(T element, int index) {
-
+        elements[size++] = element;
     }
 
     public void add(int index, T element) {
-        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
-        ensureCapacity();
-        System.arraycopy(data, index, data, index + 1, size - index);
-        data[index] = element;
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
         size++;
     }
 
+    public void addFirst(T element) {
+        add(0, element);
+    }
+
+    public void addLast(T element) {
+        add(element);
+    }
+
     public T get(int index) {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
-        return (T) data[index];
+        return (T) elements[index];
+    }
+
+    public T getFirst() {
+        return get(0);
+    }
+
+    public T getLast() {
+        return get(size - 1);
+    }
+
+    public void set(int index, T element) {
+        elements[index] = element;
     }
 
     public T remove(int index) {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
-        T removed = (T) data[index];
-        System.arraycopy(data, index + 1, data, index, size - index - 1);
+        T removed = (T) elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
         size--;
         return removed;
     }
 
-    public boolean remove(T element) {
+    public void removeFirst() {
+        remove(0);
+    }
+
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    public void clear() {
+        size = 0;
+    }
+
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    public int indexOf(Object object) {
         for (int i = 0; i < size; i++) {
-            if (data[i].equals(element)) {
-                remove(i);
-                return true;
+            if (elements[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object object) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (elements[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    public void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                Comparable<T> a = (Comparable<T>) elements[i];
+                T b = (T) elements[j];
+                if (a.compareTo(b) > 0) {
+                    Object temp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = temp;
+                }
             }
         }
-        return false;
     }
 
     public int size() {
         return size;
     }
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public void clear() {
-        size = 0;
-        data = new Object[DEFAULT_CAPACITY];
-    }
-
-    public boolean contains(T element) {
+    public Object[] toArray() {
+        Object[] array = new Object[size];
         for (int i = 0; i < size; i++) {
-            if (data[i].equals(element)) return true;
+            array[i] = elements[i];
         }
-        return false;
+        return array;
     }
 
-    public java.util.Iterator<T> iterator() {
-        return new java.util.Iterator<T>() {
-            private int current = 0;
-
-            public boolean hasNext() {
-                return current < size;
-            }
-
-            public T next() {
-                return (T) data[current++];
-            }
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int index = 0;
+            public boolean hasNext() { return index < size; }
+            public T next() { return (T) elements[index++]; }
         };
     }
 }
--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import java.util.Iterator;
+
 public interface MyList<T> extends Iterable<T> {
     void add(T element);
-    void add(T element, int index);
+    void add(int index, T element);
     T get(int index);
+    void set(int index, T element);
     T remove(int index);
-    boolean remove(T element);
-    int size();
-    boolean isEmpty();
+    void removeFirst();
+    void removeLast();
+    T getFirst();
+    T getLast();
+    void addFirst(T element);
+    void addLast(T element);
     void clear();
-    boolean contains(T element);
+    boolean exists(Object object);
+    int indexOf(Object object);
+    int lastIndexOf(Object object);
+    void sort();
+    int size();
+    Object[] toArray();
+    Iterator<T> iterator();
 }
